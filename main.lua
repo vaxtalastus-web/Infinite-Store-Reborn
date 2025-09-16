e435c139ce292c7b165f2f60ef2fa4d7f8787030
vaxtalastus-web <vaxtalastus@gmail.com>
2025-09-16 15:17:07 +1000

    Delete main.lua

--- main.lua
+++
@@ -1,3 +0,0 @@
--- security reasons
--- plus infinite store is discontinued and no longer maintained don't use it
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
